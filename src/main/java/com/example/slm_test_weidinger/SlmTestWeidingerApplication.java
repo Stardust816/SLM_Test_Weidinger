39c01f9eb194b58a70ce4ff0b0378192cf56c701
stephanweidinger <ic21b064@technikum-wien.at>
2023-01-26 18:00:25 +0100

    Initial commit

---
+++ src/main/java/com/example/slm_test_weidinger/SlmTestWeidingerApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_test_weidinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestWeidingerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTestWeidingerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slm_test_weidinger/SlmTestWeidingerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_test_weidinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestWeidingerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
